d70e78a29c20b8e265a20ccc1ecd08671dc2bb01
jyghrtd <61083737+jyghrtd@users.noreply.github.com>
2024-02-28 17:25:47 +0900

    Update db.js

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 var pp_db = mysql.createConnection({
     host: 'localhost',
     port: '3306',
-    user: 'root',
-    password: '1410620',
+    user: 'scoot',
+    password: 'tiger',
     database: 'PorkPointDB'
 })
 pp_db.connect();
